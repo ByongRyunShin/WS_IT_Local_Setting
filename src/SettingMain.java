49d44f88f05f6f63f6015043018ddd314c5131c9
ShinByongRyun <nj298@kumoh.ac.kr>
2017-03-22 10:11:56 +0900

    Create Schema & Tables

--- src/SettingMain.java
+++ src/SettingMain.java
@@ -18,6 +18,14 @@ public class SettingMain {
 				stmt.execute("drop database `" + s + "`");
 			}
 		}
+		
+		//새로운 DB 생성
+		stmt.execute("CREATE SCHEMA `sw3_01` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ;");
+		
+		//테이블 생성
+		stmt.execute("CREATE TABLE `sw3_01`.`tbl_customer` (  `cID` VARCHAR(6) NOT NULL,  `cPW` VARCHAR(4) NULL,  `cName` VARCHAR(10) NULL,  `cHP` VARCHAR(13) NULL,  PRIMARY KEY (`cID`));");
+		stmt.execute("CREATE TABLE `sw3_01`.`tbl_bus` (  `bNumber` VARCHAR(4) NOT NULL,  `bDeparture` VARCHAR(5) NULL,  `bArrival` VARCHAR(5) NULL,  `bTime` TIME NULL,  `bElapse` VARCHAR(10) NULL,  `bCount` VARCHAR(1) NULL,`bPrice` INT(6) NULL,PRIMARY KEY (`bNumber`));");
+		stmt.execute("CREATE TABLE `sw3_01`.`tbl_ticket` (  `bDate` DATE NULL,  `bNumber` VARCHAR(4) NULL,  `bNumber2` VARCHAR(5) NULL,  `bSeat` INT(2) NULL,  `cID` VARCHAR(6) NULL,  `bPrice` INT(6) NULL,  `bState` VARCHAR(1) NULL);");
 
 	}
 }
