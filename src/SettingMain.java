471ddf288ea3904d9d968ec6b3ec7a0869157ce4
ShinByongRyun <nj298@kumoh.ac.kr>
2017-03-22 10:11:05 +0900

    DB System  Reset(Existing DB Reset)

--- src/SettingMain.java
+++ src/SettingMain.java
@@ -9,5 +9,15 @@ public class SettingMain {
 		Class.forName("com.mysql.jdbc.Driver");
 		Connection con = DriverManager.getConnection("jdbc:mysql://localhost","root","1234");		
 		Statement stmt = con.createStatement();
+		
+		//기존 DB삭제
+		ResultSet rs = con.getMetaData().getCatalogs();
+		while (rs.next()) {
+			String s = rs.getString("table_cat");
+			if (!s.equals("sys") && !s.equals("information_schema") && !s.equals("mysql") && !s.equals("performance_schema")) {
+				stmt.execute("drop database `" + s + "`");
+			}
+		}
+
 	}
 }
