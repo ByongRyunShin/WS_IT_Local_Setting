88a2811e0e883c4ccf83cd7fd4dab7d7ff78f6d9
ShinByongRyun <nj298@kumoh.ac.kr>
2017-03-22 10:09:07 +0900

    DB Connection

--- src/SettingMain.java
+++ src/SettingMain.java
@@ -1,6 +1,13 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class SettingMain {
-	public static void main(String[] args){
-		
+	public static void main(String[] args) throws Exception{
+		//DB연결
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost","root","1234");		
+		Statement stmt = con.createStatement();
 	}
 }
